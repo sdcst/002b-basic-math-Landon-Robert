2d0ea55d204e224cf38e57d5c1e77b7a243d8714
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-04-05 10:54:28 -0700

    finished

--- a8.py
+++ a8.py
@@ -14,3 +14,9 @@ You may use either the ** operator or math.pow(x,y) for your exponents
 You may use either math.sqrt(x) or the exponent to the power of 0.5 for your square root
 
  '''
+
+a = 5 
+b = 8 
+C = a**2 + b**2
+c = C**0.5
+print(c)
